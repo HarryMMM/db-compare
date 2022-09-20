36f762f3e5ed2dffa16a301943f6590a8d55f310
haorui.hao <haorui.hao@accenture.com>
2022-09-20 15:53:38 +0800

    feature(init) improve logic

--- src/main/java/cn/harryai/tool/dbcompare/printer/excel/ExcelPrinter.java
+++ src/main/java/cn/harryai/tool/dbcompare/printer/excel/ExcelPrinter.java
@@ -4,7 +4,9 @@ import cn.harryai.tool.dbcompare.config.PrinterConfig;
 import cn.harryai.tool.dbcompare.enums.PrintFormat;
 import cn.harryai.tool.dbcompare.module.Column;
 import cn.harryai.tool.dbcompare.module.Comparable;
+import cn.harryai.tool.dbcompare.module.ComparisonColumnResult;
 import cn.harryai.tool.dbcompare.module.ComparisonResult;
+import cn.harryai.tool.dbcompare.module.ComparisonTableResult;
 import cn.harryai.tool.dbcompare.module.Table;
 import cn.harryai.tool.dbcompare.printer.AbsPinter;
 import cn.harryai.tool.dbcompare.printer.DataWarp;
@@ -13,6 +15,8 @@ import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.util.MapUtils;
 import com.alibaba.excel.write.metadata.WriteSheet;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -48,8 +52,20 @@ public final class ExcelPrinter extends AbsPinter<PrinterConfig> {
             excelWriter.fill(map, columnSheet);
             Map<Class<? extends Comparable>, List<? extends ComparisonResult>> compResult = dataWarp.getCompResult();
 
-            excelWriter.fill(dillData(compResult.get(Table.class)), tableSheet);
-            excelWriter.fill(dillData(compResult.get(Column.class)), columnSheet);
+            List<ComparisonTableResult> compResult1 = (List<ComparisonTableResult>) compResult.get(Table.class);
+            excelWriter.fill(dillData(compResult1), tableSheet);
+
+            // 删除表不存在的列。没有比较的意义。
+            Map<Pair<String, String>, Pair<String, String>> collect =
+                    compResult1.stream().collect(Collectors.toMap(e -> Pair.of(e.getTableSchema(), e.getTableName()),
+                            e -> Pair.of(e.getMissingL(), e.getMissingR())));
+            List<ComparisonColumnResult> compResult2 = (List<ComparisonColumnResult>) compResult.get(Column.class);
+            compResult2.removeIf(e -> {
+                Pair<String, String> stringStringPair = collect.get(Pair.of(e.getTableSchema(), e.getTableName()));
+                return stringStringPair != null && StringUtils.equalsAny("YES", stringStringPair.getLeft(),
+                        stringStringPair.getRight());
+            });
+            excelWriter.fill(dillData(compResult2), columnSheet);
         }
         return fileName.toString();
     }
